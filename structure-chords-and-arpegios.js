96d55767faf2c7e5f9a378445e2af55dda6ccec2
Gabriel RZ <40544981+gabriel96x1@users.noreply.github.com>
2025-08-01 12:18:33 -0600

    Update structure-chords-and-arpegios.js

--- structure-chords-and-arpegios.js
+++ structure-chords-and-arpegios.js
@@ -1,8 +1,11 @@
+$: s("sd").bank("tr909").beat("4,12", 16)
+$: s("bd").bank("tr909").beat("2,3,5 10,11, 13", 16)
+
 $: n(`<[0,2,4,6]
 [-1,0,2,4]
 [1,4,5,7]
 [-1,0,1,3]
->`).scale("C2:minor").s("gm_church_organ") // Acordes
+>`).scale("C2:minor").s("gm_church_organ").gain(.3) // Acordes
   ._pianoroll()
 
 $: n(`<[6 4 2 0]*4
